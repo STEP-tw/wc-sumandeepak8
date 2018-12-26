51e0c49e0abceb2111c96d25281f3a5720c34dd5
suman rajput <44019903+sumandeepak8@users.noreply.github.com>
2018-12-26 18:30:21 +0530

    Handled default case for wc .

--- src/wcLib.js
+++ src/wcLib.js
@@ -1,38 +1,46 @@
 const newLine = '\n';
 
-const fileReader = function(reader, file) {
+const fileReader = function (reader, file) {
   return reader(file, 'utf-8');
 };
 
-const spaceJustifier = function(arg) {
+const spaceJustifier = function (arg) {
   let length = arg.toString().length;
-  let space = new Array(8-length).fill(' ').join('');
-  return `${space+arg}`;
+  let space = new Array(8 - length).fill(' ').join('');
+  return `${space + arg}`;
 };
 
-const formatter = function(lines, words, chars) {
-   return `${spaceJustifier(lines)+spaceJustifier(words)+spaceJustifier(chars)}`;
+const formatter = function (lines, words, chars) {
+  return `${spaceJustifier(lines) + spaceJustifier(words) + spaceJustifier(chars)}`;
 };
 
-const countLines = (content)=> content.split(newLine).length - 1;
-const wordCounter = (content)=> content.split(/ |\n/).length - 1;
-const countBytes = (content)=> content.length;
+const countLines = (content) => content.split(newLine).length - 1;
 
-const countWordsOutputDetails = function(content) {
-  let lines =  countLines(content);
+const wordCounter = function (content) {
+  let words = content.split(/ |\n/);
+  words = words.filter((x) => {
+    return x != '';
+  });
+  return words.length;
+};
+
+const countBytes = (content) => content.length;
+
+const countWordsOutputDetails = function (content) {
+  let lines = countLines(content);
   let words = wordCounter(content);
   let chars = countBytes(content);
   return formatter(lines, words, chars);
 };
 
-const wc = function(file, fs) {
+const wc = function (file, fs) {
   let { readFileSync } = fs;
   let readContent = fileReader.bind(null, readFileSync);
-  let fileContent = readContent(file);  
-  return `${countWordsOutputDetails(fileContent)} ${file}`; 
+  let fileContent = readContent(file);
+  return `${countWordsOutputDetails(fileContent)} ${file}`;
 };
 
 module.exports = {
-  countWordsOutputDetails, 
+  countWordsOutputDetails,
   wc
 };
